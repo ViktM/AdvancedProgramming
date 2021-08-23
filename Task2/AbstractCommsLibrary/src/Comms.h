2eeee4259006fb1ec453d540ae2b4a897565054c
Viktoria Mezei <viktoria.mezei@sky.uk>
2021-08-23 13:55:42 +0100

    Abstract library for Comms interface.

---
+++ Task2/AbstractCommsLibrary/src/Comms.h
@@ -0,0 +1,17 @@
+//
+// Created by Mezei, Szilvia (Associate Software Developer) on 23/08/2021.
+//
+
+#ifndef ABSTRACTCOMMSLIBRARY_COMMS_H
+#define ABSTRACTCOMMSLIBRARY_COMMS_H
+
+
+class Comms {
+public:
+    Comms() {};
+
+    virtual int Initialise() = 0;
+};
+
+
+#endif //ABSTRACTCOMMSLIBRARY_COMMS_H
